813bc11848b08f3a95ac4c1abc7234eb72187ac2
Porofly <yeogyeom1@naver.com>
2025-01-20 07:55:40 +0000

    init hpp

---
+++ src/jfi_transport/include/jfi_transport.hpp
@@ -0,0 +1,28 @@
+#ifndef SERIAL_COMM_NODE_HPP
+#define SERIAL_COMM_NODE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <thread>
+#include <atomic>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include "mavlink/common/mavlink.h"
+
+class SerialCommNode : public rclcpp::Node {
+public:
+    SerialCommNode();
+    ~SerialCommNode();
+
+private:
+    int serial_fd_;
+    std::thread receive_thread_;
+    std::thread send_thread_;
+    std::atomic<bool> running_; // Flag to control thread execution
+
+    void configure_serial_port();
+    void receive_loop();
+    void send_loop();
+};
+
+#endif // SERIAL_COMM_NODE_HPP
