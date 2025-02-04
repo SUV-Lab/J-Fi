83c9fa0891d01cde8190dfb9be888f7b0d4fae42
Porofly <yeogyeom1@naver.com>
2025-02-04 05:36:03 +0000

    separating main.cpp file

--- src/jfi_comm/src/serial_comm_node.cpp
+++ src/jfi_comm/src/serial_comm_node.cpp
@@ -63,11 +63,3 @@ void SerialCommNode::handleMessage(const int tid, const std::vector<uint8_t> & d
   }
 }
 
-int main(int argc, char** argv)
-{
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<SerialCommNode>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
